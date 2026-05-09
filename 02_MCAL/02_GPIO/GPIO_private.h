507d08d70cd1313862bf762162fa53adece3bf92
MohamedAldreamly <20232816@student.alazhar.edu.ps>
2026-05-09 11:39:30 +0300

    Update GPIO_private.h

--- 02_MCAL/02_GPIO/GPIO_private.h
+++ 02_MCAL/02_GPIO/GPIO_private.h
@@ -15,17 +15,7 @@
 #define GPIO_PRIVATE_H
 
 
-typedef struct
-{
-    volatile u32 GPIO_CRL;
-    volatile u32 GPIO_CRH;
-    volatile u32 GPIO_IDR;
-    volatile u32 GPIO_ODR;
-    volatile u32 GPIO_BSRR;
-    volatile u32 GPIO_BRR;
-    volatile u32 GPIO_LCKR;
 
-} GPIO_Typedef;
 
 #define GPIOA        ((GPIO_Typedef*)0x40010800)            // define the boundary address of GPIO port A
 #define GPIOB        ((GPIO_Typedef*)0x40010C00)            // define the boundary address of GPIO port B
